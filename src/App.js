0c4b5dd10064a7fd0440ecaf6b25a4f6bffc6082
qiyu1987 <qiyu1987@gmail.com>
2019-10-28 22:20:18 +0100

    clean up app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import store from './store';
-import { Provider } from 'react-redux';
 import { connect } from 'react-redux';
 import { Route, Link } from 'react-router-dom';
 import Home from './components/Home';
@@ -9,16 +7,17 @@ import Login from './components/Login';
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <div>
-          <Link to='/'>Home</Link>
-          <Link to='/signup'>Sign up</Link>
-          <Link to='/login'>Login</Link>
-          <Route path='/' exact component={Home} />
-          <Route path='/signup' exact component={SignUp} />
-          <Route path='/login' exact component={Login} />
-        </div>
-      </Provider>
+      <div>
+        <Link to='/'>Home</Link>
+        <br/>
+        <Link to='/signup'>Sign up</Link>
+        <br/>
+        <Link to='/login'>Login</Link>
+        <br/>
+        <Route path='/' exact component={Home} />
+        <Route path='/signup' exact component={SignUp} />
+        <Route path='/login' exact component={Login} />
+      </div>
     );
   }
 }
