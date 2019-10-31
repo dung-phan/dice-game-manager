5c8540eccb556d02f25c4804a81780b60ac1cb9c
qiyu1987 <qiyu1987@gmail.com>
2019-10-31 11:59:28 +0100

    localstorage userId token

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,10 +1,13 @@
 import { USER_LOGIN } from '../actions/login';
+const token = localStorage.getItem("jwt");
+const initialState = token ? token : null;
 
-export default (state = '', action = {}) => {
+export default (state = initialState, action = {}) => {
   console.log('check reducer state', state);
   console.log('check action', action);
   switch (action.type) {
     case USER_LOGIN:
+      localStorage.setItem("jwt", action.payload.jwt);
       return action.payload.jwt;
     default:
       return state;
