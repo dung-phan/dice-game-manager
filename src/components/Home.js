af3e8dae9e55a9a8c6df91440c52e072503259c4
dung-phan <madewithcrayons@gmail.com>
2019-12-17 17:20:33 +0100

    customise css for homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-
+import Navigation from './Navigation';
 function Home(props) {
   return (
-    <div>
-       {!props.loggedIn ? (
-        <div>
-          <Link to='/signup'>Are you new? Sign up here! </Link>
-          <br />
-          <Link to='/login'>Log in</Link>
+    <div className="section-home">
+      <div className="row">
+        <div className="frame">
+          <div className="frame__left">
+            <Navigation />
+            <div className="heading__secondary">Dice</div>
+            <div className="heading__secondary"> Game</div>
+            <p>
+              Have fun rolling dices with friends, go into a new world and take
+              risks together.
+            </p>
+            <br></br>
+            <button className="btn btn-main btn-animated">
+              Join <i className="icon ion-md-person-add"></i>
+            </button>
+          </div>
+          <div className="frame__right">
+            <div className="frame__right--box"></div>
+          </div>
         </div>
-      ) : ('You are logged in')}
-      <br />
-      <Link to='/lobby'>To the game lobby</Link>
+      </div>
     </div>
   );
 }
